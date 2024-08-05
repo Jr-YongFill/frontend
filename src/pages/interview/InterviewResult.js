55f8e416d163d00487c33215888706f4e0a5d009
Changchang <bbmini96@hanmail.net>
2024-08-05 16:40:30 +0900

    Fix: #73 inverviewResult CSS 수정

--- src/pages/interview/InterviewResult.js
+++ src/pages/interview/InterviewResult.js
@@ -9,22 +9,36 @@ import {
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import styled from 'styled-components';
 import Header from "../../components/Header";
-import img from "../../assets/default.png";
+import img from "../../assets/note.png";
 import CustomButton from "../../components/CustomButton";
 import { useLocation, useNavigate } from "react-router-dom";
 import pallete from "../../styles/pallete";
 import Wrapper from '../../components/Wrapper';
 import GlassCard from '../../components/GlassCard';
+import palette from '../../styles/pallete';
+import Block from '../../components/Block';
 
 
-const TitleContainer = styled(Box)`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    max-width: 1200px;
-    margin: 50px auto;
-    padding: 0 20px;
+const SubTitleText = styled.div`
+  margin-top: 40px;
+  font-size: 0.8em;
+  font-weight: bold;
 `;
+
+const BtnGroup = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  margin: 4vh 8vw;
+`;
+
+const BtnBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content:center;
+`;
+
 
 const InterviewNote = () => {
   const [expanded, setExpanded] = useState(false);
@@ -37,45 +51,55 @@ const InterviewNote = () => {
   };
 
   return (
-    <>
+    <div>
       <Header />
       <Wrapper>
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <TitleContainer>
-            <div style={{
-              width: '100%',
-              display: 'flex',
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              margin: '20px'
-            }}>
-              <div>
-                <Typography variant="h2" marginBottom={10}>
-                  면접 결과
-                </Typography>
-                <Typography variant="h5">
-                  과연 당신은 면접을 잘 봤을까요? <br />
-                  GPT는 아니라고 하던데,,,
-                </Typography>
+        <div style={{ margin: "auto 0" }}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <Block></Block>
+            <GlassCard width={"60vw"}>
+              <div
+                style={{
+                  height: "20vh",
+                  margin: "40px 8vw",
+                  display: "flex",
+                  justifyContent: "space-between",
+                  alignItems: "center",
+                }}
+              >
+                <div>
+                  <h1>면접 결과</h1>
+                  <SubTitleText>
+                    <h1>면접 잘 보셨나요?</h1>
+                  </SubTitleText>
+                </div>
+                <img src={img} width={"18%"} alt={"기본 이미지"} style={{ marginRight: '30px' }}></img>
               </div>
-              <img src={img} width={'40%'} alt={'기본 이미지'} />
-            </div>
-            <div style={{ display: 'flex', width: '100%', justifyContent: 'space-around' }}>
-              <CustomButton sx={{ marginTop: 2 }} onClick={() => navigate('/interview/main')}>재도전</CustomButton>
-              <CustomButton sx={{ marginTop: 2 }} onClick={() => navigate('/interview/note')}>오답노트</CustomButton>
-            </div>
-          </TitleContainer>
-          <GlassCard>
+              <BtnGroup>
+                <BtnBox>
+                  <CustomButton onClick={() => navigate('/interview/main')}>
+                    재도전
+                  </CustomButton>
+                </BtnBox>
+                <BtnBox>
+                  <CustomButton onClick={() => navigate('/interview/note')}>
+                    오답노트
+                  </CustomButton>
+                </BtnBox>
+
+              </BtnGroup>
+            </GlassCard>
+
             {answers.length > 0 ? answers.map((answer, index) => (
               <Accordion
                 sx={{
                   '&:before': { display: 'none' },
                   marginBottom: 2,
-                  color: 'darkslategray',
-                  backgroundColor: '#F0F0F0',
-                  borderRadius: '0.6rem',
-                  boxShadow: 'none',
-                  maxWidth: '1200px'
+                  color: 'white',
+                  background: 'rgba(255, 255, 255, 0.1)',
+                  backdropFilter: 'blur(10px)',
+                  boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
+                  borderRadius: '10px'
                 }}
                 key={index}
                 expanded={expanded === index}
@@ -85,26 +109,23 @@ const InterviewNote = () => {
                   <Typography variant="h6">{index + 1}. {answer.question}</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                  <GlassCard sx={{ display: 'flex', backgroundColor: pallete.pink, borderRadius: '20px', marginBottom: '20px' }}>
-                    <GlassCard padding={2} sx={{ flex: 1 }}>
-                      <Typography variant="h5" gutterBottom>나의 답변</Typography>
-                      <Typography>{answer.memberAnswer}</Typography>
-                    </GlassCard>
-                    <GlassCard padding={2} sx={{ flex: 1 }}>
-                      <Typography variant="h5" gutterBottom>GPT의 답변</Typography>
-                      <Typography>{answer.gptAnswer}</Typography>
-                    </GlassCard>
+                  <GlassCard width={"58vw"} padding={2} sx={{ flex: 1 }}>
+                    <Typography variant="h5" gutterBottom>나의 답변</Typography>
+                    <Typography>{answer.memberAnswer}</Typography>
+                  </GlassCard>
+                  <GlassCard width={"58vw"} padding={2} sx={{ flex: 1 }}>
+                    <Typography variant="h5" gutterBottom>GPT의 답변</Typography>
+                    <Typography>{answer.gptAnswer}</Typography>
                   </GlassCard>
                 </AccordionDetails>
               </Accordion>
             ))
               :
               '답변한 내역이 없습니다.'}
-          </GlassCard>
+          </div>
         </div>
-
       </Wrapper>
-    </>
+    </div>
   );
 };
 
